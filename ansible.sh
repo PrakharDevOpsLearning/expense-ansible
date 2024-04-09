e84908216bd29fadfc6a55e3130572f3ae2fa8b2
prkhar <prkharpatel20@gmail.com>
2024-04-09 21:43:29 +1000

    vault integration to entire project (ansible and Terrafrom)along with public subnets creation

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,4 @@
 component=$1
-
-ansible-playbook -i $component-$env.prakhardevopslearning.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 expense.yml -e role_name=$component -e env=$env
+ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
+ansible-playbook -i $component-$env.prakhardevopslearning.online, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
+rm -f ~/secrets.json ~/app.json
